86af015131b15034fae4727da7ceb95cf88e7f5c
robert <roberthshields93@yahoo.com>
2021-07-05 17:44:57 -0700

    social page done

--- src/components/Social/Social.jsx
+++ src/components/Social/Social.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 import useStyles from './styles';
 import coffee from '../../assets/coffee.mp4'
-import { Toolbar } from '@material-ui/core';
+import { Toolbar, Typography } from '@material-ui/core';
+import Github from '../../assets/github.png'
+
 
 const Social = () => {
    const classes = useStyles()
@@ -10,15 +12,21 @@ const Social = () => {
    return (
       <>
          <Toolbar disableGutters/>
-         <div className={classes.container}>
-            <div className={classes.card}>
-               hello
-            </div>
-            <div className={classes.card}>
-               hello
-            </div>
-            <div className={classes.card}>
-               hello
+         <div className={classes.center}>
+            <Typography variant='h3' align='center'>Feel free to add/message me!</Typography>
+            <div className={classes.container}>
+               <a href='https://github.com/TheRealRobertShields' target='_blank' without rel="noreferrer" className={classes.card}>
+                  <img src={Github} alt='github' />
+                  <p>TheRealRobertShields</p>
+               </a>
+               <a href='https://www.linkedin.com/in/robert-shields-4b70541b6/' target='_blank' without rel="noreferrer" className={classes.card}>
+                  <img src="https://img.icons8.com/color/48/000000/linkedin.png" alt='linkedin' />
+                  <p>Robert Shields</p>
+               </a>
+               <a href='mailto:robert.shields.dev@gmail.com?subject=Hello Robert!&body=My name is ...' className={classes.card}>
+                  <img src="https://img.icons8.com/color/48/000000/gmail-new.png" alt='github' />
+                  <p>robert.shields.dev</p>
+               </a>
             </div>
          </div>
 
--- src/components/Social/styles.js
+++ src/components/Social/styles.js
@@ -1,24 +1,45 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
+   center: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '90vh',
+      width: '90vw',
+   },
    container: {
       display: 'flex',
       flexDirection: 'row',
       flexWrap: 'wrap',
       justifyContent: 'center',
       alignItems: 'center',
-      height: '90vh',
       width: '90vw',
    },
    card: {
       display: 'flex',
-      flexDirection: 'row',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       width: '150px',
       height: '200px',
-      backgroundColor: 'rgba(255, 255, 255, 0.25)',
-      margin: '20px',
+      margin: '20px',  
+      backgroundColor: 'rgba(255, 255, 255, 0.15)',
+      borderRadius: '4px',
+      borderLeft: '1px solid rgba(255, 255, 255, 0.15)',
+      borderTop: '1px solid rgba(255, 255, 255, 0.15)',
+      borderRight: '1px solid rgba(255, 255, 255, 0.05)',
+      borderBottom: '1px solid rgba(255, 255, 255, 0.05)',
+      fontSize: 'calc(8px + 0.25vw)',
+      fontWeight: '100',
+      color: 'white',
+      textDecoration: 'none',
+      transition: 'all 0.3s',
+      '&:hover': {
+         background: 'rgba(255, 255, 255, 0.2)',
+         transform: 'scale(1.1)'
+      },
    },
    video: {
       zIndex: -1,
