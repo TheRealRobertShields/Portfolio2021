857b1b1ea2ed089a1710b37a480af2a50cf6cb6a
robert <roberthshields93@yahoo.com>
2021-07-08 23:36:39 -0700

    links fixed

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -20,19 +20,19 @@ const Projects = () => {
       <div className={classes.center}>
          <Typography variant='h3' align='center' style={{fontSize:'calc(8px + 1vw)', width:'50%'}}>Check out some of my projects!</Typography>
          <div className={classes.container}>
-            <a href='https://github.com/TheRealRobertShields' target='_blank' without rel="noreferrer" className={classes.card}>
+            <a href='https://fotogrid.netlify.app/' target='_blank' without rel="noreferrer" className={classes.card}>
                <img src={fotogrid} alt='fotogrid' style={{width: 'calc(150px + 10vw)'}} />
                <p>TheRealRobertShields</p>
             </a>
-            <a href='https://github.com/TheRealRobertShields' target='_blank' without rel="noreferrer" className={classes.card}>
+            <a href='https://fotogrid.netlify.app/' target='_blank' without rel="noreferrer" className={classes.card}>
                <img src={fotogrid} alt='fotogrid' style={{width: 'calc(150px + 10vw)'}} />
                <p>TheRealRobertShields</p>
             </a>
-            <a href='https://github.com/TheRealRobertShields' target='_blank' without rel="noreferrer" className={classes.card}>
+            <a href='https://fotogrid.netlify.app/' target='_blank' without rel="noreferrer" className={classes.card}>
                <img src={fotogrid} alt='fotogrid' style={{width: 'calc(150px + 10vw)'}} />
                <p>TheRealRobertShields</p>
             </a>
-            <a href='https://github.com/TheRealRobertShields' target='_blank' without rel="noreferrer" className={classes.card}>
+            <a href='https://fotogrid.netlify.app/' target='_blank' without rel="noreferrer" className={classes.card}>
                <img src={fotogrid} alt='fotogrid' style={{width: 'calc(150px + 10vw)'}} />
                <p>TheRealRobertShields</p>
             </a>
