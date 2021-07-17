d6e02138f410aac5109bcf746e5c1912718129eb
robert <roberthshields93@yahoo.com>
2021-07-17 16:37:35 -0700

    new link added to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,6 +4,7 @@ import { Typography } from '@material-ui/core';
 
 // import thinking from '../../assets/thinking.mp4'
 import fotogrid from '../../assets/fotogrid_thumbnail.png'
+import okisfreezedried from '../../assets/okisfreezedried.png'
 import useStyles from './styles';
 
 const Projects = () => {
@@ -20,17 +21,9 @@ const Projects = () => {
       <div className={classes.center}>
          <Typography variant='h3' align='center' style={{fontSize:'calc(8px + 1vw)', width:'50%'}}>Check out some of my projects!</Typography>
          <div className={classes.container}>
-            <a href='https://fotogrid.netlify.app/' target='_blank' without rel="noreferrer" className={classes.card}>
-               <img src={fotogrid} alt='fotogrid' style={{width: 'calc(150px + 10vw)'}} />
-               <p>FotoGrid</p>
-            </a>
-            <a href='https://fotogrid.netlify.app/' target='_blank' without rel="noreferrer" className={classes.card}>
-               <img src={fotogrid} alt='fotogrid' style={{width: 'calc(150px + 10vw)'}} />
-               <p>FotoGrid</p>
-            </a>
-            <a href='https://fotogrid.netlify.app/' target='_blank' without rel="noreferrer" className={classes.card}>
-               <img src={fotogrid} alt='fotogrid' style={{width: 'calc(150px + 10vw)'}} />
-               <p>FotoGrid</p>
+            <a href='https://okisfreezedried.com' target='_blank' without rel="noreferrer" className={classes.card}>
+               <img src={okisfreezedried} alt='okisfreezedried' style={{width: 'calc(150px + 10vw)'}} />
+               <p>okisfreezedried</p>
             </a>
             <a href='https://fotogrid.netlify.app/' target='_blank' without rel="noreferrer" className={classes.card}>
                <img src={fotogrid} alt='fotogrid' style={{width: 'calc(150px + 10vw)'}} />
