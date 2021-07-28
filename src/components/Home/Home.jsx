42bd3128ca19c821e54b74c92bcb357a1f685a7e
robert <roberthshields93@yahoo.com>
2021-07-27 23:15:43 -0700

    fixed animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,13 +3,18 @@ import { Typography } from '@material-ui/core';
 
 // import paddling from '../../assets/paddling.mp4'
 import useStyles from './styles';
+import { Container } from '@material-ui/core';
 
 const Home = () => {
    const classes = useStyles()
 
    return (
       <div className={classes.center}>
-         <Typography variant='h3' align='center' className='titleAnimated' style={{fontSize:'calc(8px + 1vw)', width:'50%'}}>Developer.</Typography>
+         <Container className={classes.titleContainer}>
+            <Typography variant='h3' align='center' className='titleAnimated' style={{fontSize:'calc(8px + 1vw)', width:'50%'}}></Typography>
+            <Typography variant='h3' align='center' className='title' style={{fontSize:'calc(8px + 1vw)', width:'50%'}}>Developer.</Typography>
+         </Container>
+
          
       </div>
    )
